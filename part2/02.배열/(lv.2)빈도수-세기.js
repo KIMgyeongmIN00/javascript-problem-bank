f7b1a3a85a5cba7d07a7a30fa11739dcd96c048c
김경민 <gimgyeongmin@MacBookPro.Davolink>
2025-02-13 23:25:04 +0900

    250213

--- part2/02.배열/(lv.2)빈도수-세기.js
+++ part2/02.배열/(lv.2)빈도수-세기.js
@@ -11,7 +11,11 @@
  */
 
 // TODO: 함수를 작성하세요.
-function getFrequency(arr) {}
+function getFrequency(arr) {
+  let freqMap = new Map();
+  arr.forEach((e) => { freqMap.set(e, (freqMap.get(e) || 0) + 1) });
+  return Object.fromEntries(freqMap)
+}
 
 // export 를 수정하지 마세요.
 export { getFrequency };
