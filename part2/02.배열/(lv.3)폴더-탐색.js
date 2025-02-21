8b1bbb16c4d0bcb3d0a0aa852c18ada4b6becc90
KGyeong <rlarudals61@naver.com>
2025-02-21 21:06:01 +0900

    250221

--- part2/02.배열/(lv.3)폴더-탐색.js
+++ part2/02.배열/(lv.3)폴더-탐색.js
@@ -18,7 +18,13 @@
  */
 
 // TODO: getAllFolderNames 함수를 작성하세요.
-function getAllFolderNames(folder) {}
+function getAllFolderNames(folder) {
+  let result = [folder.name];
+  folder.subFolders.forEach(subFolder => {
+    result = result.concat(getAllFolderNames(subFolder));
+  });
+  return result;
+}
 
 // export 를 수정하지 마세요.
 export { getAllFolderNames };
