58ac01088342e356cc76baab3714d380b7c60f7d
KGyeong <rlarudals61@naver.com>
2025-03-19 09:59:28 +0900

    250318

--- part2/04.클로저/(lv.3)throttle.js
+++ part2/04.클로저/(lv.3)throttle.js
@@ -11,7 +11,17 @@
  */
 
 // TODO: throttle 함수를 작성하세요.
-function throttle(func, delay) {}
+function throttle(func, delay) {
+  let running
+  return (...args) => {
+    if (running) return
+    running = true
+    func(...args)
+    setTimeout(() => {
+      running = false
+    }, delay);
+  }
+}
 
 // export 를 수정하지 마세요.
 export { throttle };
